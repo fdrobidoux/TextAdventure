6123db322695a55d696258b0316be0198acfb084
Félix Dion Robidoux <felix@fdrobidoux.com>
2019-11-07 16:54:55 -0500

    Tried to code test entities console view.

--- TextAdventure.Core/Consoles/Tests/TestAnimatedEntitiesConsole.cs
+++ TextAdventure.Core/Consoles/Tests/TestAnimatedEntitiesConsole.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
 using SadConsole;
+using SadConsole.Entities;
 
 namespace TextAdventure.Core.Consoles.Tests
 {
@@ -8,7 +10,21 @@ namespace TextAdventure.Core.Consoles.Tests
     {
         public TestAnimatedEntitiesConsole() : base(Global.CurrentScreen.Width, Global.CurrentScreen.Height)
         {
+            Font = Global.Fonts["EntityGuy"].GetFont(Font.FontSizes.One);
 
+            AnimatedConsole animWalkRight = new AnimatedConsole("WalkRight", 1, 1, Font);
+
+            for (int i = 2; i < 10; i++)
+            {
+                CellSurface frame = animWalkRight.CreateFrame();
+                frame.Cells[0].Glyph = i;
+                frame.Cells[0].Mirror = SpriteEffects.FlipHorizontally;
+            }
+
+            Entity stickman = new Entity(1, 1, Font);
+            stickman.Animations.Add("WalkRight", animWalkRight);
+
+            stickman.Animation = stickman.Animations["WalkRight"];
         }
     }
 }
