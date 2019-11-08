3f4c1f22f34a309df4e92a3223eafbaea2f57d9d
Félix Dion-Robidoux <felix@fdrobidoux.com>
2019-11-07 23:17:17 -0500

    Tried to fiddle with EntityGuy spritefont that won't work...

--- TextAdventure.Core/Consoles/Tests/TestAnimatedEntitiesConsole.cs
+++ TextAdventure.Core/Consoles/Tests/TestAnimatedEntitiesConsole.cs
@@ -8,23 +8,31 @@ namespace TextAdventure.Core.Consoles.Tests
 {
     public class TestAnimatedEntitiesConsole : SadConsole.Console
     {
-        public TestAnimatedEntitiesConsole() : base(Global.CurrentScreen.Width, Global.CurrentScreen.Height)
+        public TestAnimatedEntitiesConsole() : base(30, 10)
         {
-            Font = Global.Fonts["EntityGuy"].GetFont(Font.FontSizes.One);
+            Font = Global.Fonts["EntityGuy"].GetFont(Font.FontSizes.Two);
+
+            SetGlyph(0, 0, 3);
+
+            AnimatedConsole animIdle = new AnimatedConsole("default", 1, 1, Font);
+
+            animIdle.CreateFrame().Cells[0].Glyph = 2;
 
             AnimatedConsole animWalkRight = new AnimatedConsole("WalkRight", 1, 1, Font);
 
-            for (int i = 2; i < 10; i++)
+            for (int i = 2; i < 11; i++)
             {
                 CellSurface frame = animWalkRight.CreateFrame();
                 frame.Cells[0].Glyph = i;
                 frame.Cells[0].Mirror = SpriteEffects.FlipHorizontally;
             }
 
-            Entity stickman = new Entity(1, 1, Font);
+            Entity stickman = new Entity(animIdle);
             stickman.Animations.Add("WalkRight", animWalkRight);
 
-            stickman.Animation = stickman.Animations["WalkRight"];
+            stickman.Animation = animIdle;
+
+            Children.Add(stickman);
         }
     }
 }
